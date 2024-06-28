d8f8bd7462d496ca242e30f10f766f61012f69d3
James Murphy <jmurphy98@qub.ac.uk>
2024-06-28 16:51:12 +0100

    further cards added adding card widget now

--- lib/data/card_data.dart
+++ lib/data/card_data.dart
@@ -16,4 +16,28 @@ const availMessageCards = [
     title: 'No', 
     imagePath: 'assets/images/no.png'
   ),
+  MessageCard(
+    title: 'Maybe', 
+    imagePath: 'assets/images/maybe.png'
+  ),
+  MessageCard(
+    title: 'Don\'t know', 
+    imagePath: 'assets/images/dont_know.png'
+  ),
+  MessageCard(
+    title: 'Please', 
+    imagePath: 'assets/images/please.png'
+  ),
+  MessageCard(
+    title: 'Thank you', 
+    imagePath: 'assets/images/thank_you.png'
+  ),
+  MessageCard(
+    title: 'Stop', 
+    imagePath: 'assets/images/stop.png'
+  ),
+  MessageCard(
+    title: 'Wait', 
+    imagePath: 'assets/images/wait.png'
+  ),
 ];
